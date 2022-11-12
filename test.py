44dbb52e0ed0c8123237556286d9a981d4b4ec4b
Malachi Massey <mm204@uark.edu>
2022-11-11 22:40:24 -0600

    Successful implmentation of image comparison. Database additions.

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-import whisper
+import face_recognition
 
-model = whisper.load_model("small")
-result = model.transcribe("game.wav")
+known_image = face_recognition.load_image_file("Database/matt-damon.jpg")
+unknown_image = face_recognition.load_image_file("Database/brad-unknown.jpg")
+
+known_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+print(results)
